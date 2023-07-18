45ae59a3cfba76ddca6a56b0654d9f51ba6e8730
JaeBEE <johnnae908@yahoo.com>
2023-07-17 17:39:13 -0700

    edit html

---
+++ assests/script.js
@@ -0,0 +1,29 @@
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+$(".time-block").each(function() {
+  var currentTime = moment().hours();
+  var timeBlock = parseInt($(this).attr("id").split("-")[1]);
+  if (timeBlock < currentTime) {
+    $(this).addClass("past");
+  } else if (timeBlock === currentTime) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("future");
+  }
+});
+
+
+$(".description").each(function() {
+  var timeBlock = $(this).parent().attr("id");
+  var savedData = localStorage.getItem(timeBlock);
+  if (savedData) {
+    $(this).val(savedData);
+  }
+});
+
+
+$(".saveBtn").on("click", function() {
+  var timeBlock = $(this).parent().attr("id");
+  var description = $(this).siblings(".description").val();
+  localStorage.setItem(timeBlock, description);
+});
